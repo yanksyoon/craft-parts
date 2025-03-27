8b9f341fb022262299ef99c479bd59cbb19cdfec
Yanks Yoon <yangsoo.yoon@canonical.com>
2025-03-27 05:34:56 +0000

    params testing

--- tests/integration/plugins/test_gradle.py
+++ tests/integration/plugins/test_gradle.py
@@ -39,12 +39,7 @@ from craft_parts import LifecycleManager, Step
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-def test_gradle_plugin(new_dir, monkeypatch, partitions):
-    """Test Gradle plugin with two cases.
-
-    1. When gradlew executable file exists: use gradlew.
-    2. When gradlew executable file does not exist: use system gradle.
-    """
+def test_gradle_plugin_gradlew(new_dir, monkeypatch, partitions):
     source_location = Path(__file__).parent / "test_gradle"
     monkeypatch.chdir(source_location)
     monkeypatch.setenv("http_proxy", "abc")
@@ -65,7 +60,6 @@ def test_gradle_plugin(new_dir, monkeypatch, partitions):
     )
     parts = yaml.safe_load(parts_yaml)
 
-    # 1. When gradlew executable file exists: use gradlew.
     lf = _execute_plugin(parts=parts, new_dir=new_dir, partitions=partitions)
     prime_dir = lf.project_info.prime_dir
     java_binary = Path(prime_dir, "bin", "java")
@@ -75,10 +69,6 @@ def test_gradle_plugin(new_dir, monkeypatch, partitions):
         [str(java_binary), "-jar", f"{prime_dir}/jar/HelloWorld-1.0.jar"], text=True
     )
     assert output.strip() == "Hello from Gradle-built Java"
-
-    # 2. When gradlew executable file does not exist: use system gradle.
-    (source_location / "gradlew").unlink(missing_ok=True)
-    lf = _execute_plugin(parts=parts, new_dir=new_dir, partitions=partitions)
 
 
 def _execute_plugin(parts, new_dir, partitions) -> LifecycleManager:
@@ -95,3 +85,35 @@ def _execute_plugin(parts, new_dir, partitions) -> LifecycleManager:
         ctx.execute(actions)
 
     return lf
+
+
+def test_gradlew_plugin_gradle(new_dir, monkeypatch, partitions):
+    source_location = Path(__file__).parent / "test_gradle"
+    monkeypatch.chdir(source_location)
+    monkeypatch.setenv("http_proxy", "abc")
+    monkeypatch.setenv("https_proxy", "def")
+    monkeypatch.setenv("no_proxy", "localhost")
+    parts_yaml = textwrap.dedent(
+        f"""
+        parts:
+          foo:
+            plugin: gradle
+            gradle-task: build
+            source: {source_location}
+            build-packages: [gradle, default-jdk]
+            stage-packages: [default-jre-headless]
+        """
+    )
+    parts = yaml.safe_load(parts_yaml)
+    (source_location / "gradlew").unlink(missing_ok=True)
+
+    lf = _execute_plugin(parts=parts, new_dir=new_dir, partitions=partitions)
+
+    prime_dir = lf.project_info.prime_dir
+    java_binary = Path(prime_dir, "bin", "java")
+    assert java_binary.is_file()
+
+    output = subprocess.check_output(
+        [str(java_binary), "-jar", f"{prime_dir}/jar/HelloWorld-1.0.jar"], text=True
+    )
+    assert output.strip() == "Hello from Gradle-built Java"
